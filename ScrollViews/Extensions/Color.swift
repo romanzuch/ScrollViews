939569c56869f8e85d06530dbc5b52cb18422fa0
Roman <roman.zuchowski@gmail.com>
2024-02-25 20:23:54 +0100

    adds color extension
    for random colors

---
+++ ScrollViews/Extensions/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  ScrollViews
+//
+//  Created by Roman Zuchowski on 25.02.24.
+//
+
+import SwiftUI
+
+public extension Color {
+
+    static func random(randomOpacity: Bool = false) -> Color {
+        Color(
+            red: .random(in: 0...1),
+            green: .random(in: 0...1),
+            blue: .random(in: 0...1),
+            opacity: randomOpacity ? .random(in: 0...1) : 1
+        )
+    }
+}
